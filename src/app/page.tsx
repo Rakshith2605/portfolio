d4c81b55f018ceed0adb5c09378d447b95613123
Rakshith2605 <rakshith.d26@outlook.com>
2025-07-20 18:42:24 -0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import FluidCursor from '@/components/FluidCursor';
 import { Button } from '@/components/ui/button';
-import LikesCounter from '@/components/likes-counter';
 
 import WelcomeModal from '@/components/welcome-modal';
 import { motion, AnimatePresence } from 'framer-motion';
@@ -259,8 +258,6 @@ export default function Home() {
             ))}
           </div>
 
-          {/* Likes Counter - positioned in top right corner */}
-          <LikesCounter />
         </motion.div>
 
         {/* Chat Overlay */}
