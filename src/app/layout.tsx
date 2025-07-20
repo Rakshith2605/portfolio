3b263794327f5950d5899937265c710973ff38ef
Rakshith2605 <rakshith.d26@outlook.com>
2025-07-20 10:45:43 -0500

    fevicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,12 +50,12 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/favicon.svg",
+        url: "/fevicon2.svg",
         sizes: "any",
       }
     ],
-    shortcut: "/favicon.svg?v=2",
-    apple: "/apple-touch-icon.svg?v=2",
+    shortcut: "/fevicon2.svg?v=2",
+    apple: "/fevicon2.svg?v=2",
   },
 };
 
@@ -68,7 +68,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        <link rel="icon" href="/favicon.svg" sizes="any" />
+        <link rel="icon" href="/fevicon2.svg" sizes="any" />
       </head>
       <body
         className={cn(
